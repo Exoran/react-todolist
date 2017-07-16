40b5f956d99d8553ae7c0f699d64cf9d93e7d06f
Alexandre <alexandre44.robin@gmail.com>
2017-07-16 12:32:22 +0200

    Part two : initialized the Todo Component

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,16 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+
+/*describe('TodoList App', () => {
+  it('Should load with the right title', () => {
+    expect(actualTitle).to.eql('Todo List');
+  });
+
+  it('Should allow me to create a Todo', () => {
+    const todoText = 'Get better at testing';
+
+    expect(actual).to.equal(todoText);
+  });
+});*/
